1449ae245199bcd9a5dc072d0f81c554e896410d
Smriti Dhiman <sdhiman@ualberta.ca>
2020-10-24 21:50:50 -0600

    add book screen

--- code/iBook/app/src/main/java/com/example/ibook/MainActivity.java
+++ code/iBook/app/src/main/java/com/example/ibook/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ibook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
