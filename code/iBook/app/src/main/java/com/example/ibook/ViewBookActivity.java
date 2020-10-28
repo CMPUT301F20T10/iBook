58dea0c624fd6bfb94ee4bc970dddf870e98ba54
ZjTan4 <mk12ss@outlook.com>
2020-10-28 03:56:46 -0600

    update ViewBookActivity

--- code/iBook/app/src/main/java/com/example/ibook/ViewBookActivity.java
+++ code/iBook/app/src/main/java/com/example/ibook/ViewBookActivity.java
@@ -2,6 +2,7 @@ package com.example.ibook;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -11,19 +12,21 @@ public class ViewBookActivity extends AppCompatActivity {
     private User user;
     private Book book;
 
-    TextView bookNameTextView;
-    TextView authorTextView;
-    TextView dateTextView;
-    TextView isbnTextView;
+    private TextView bookNameTextView;
+    private TextView authorTextView;
+    private TextView dateTextView;
+    private TextView isbnTextView;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_book);
 
         Intent intent = getIntent();
         this.book = (Book) intent.getSerializableExtra("BOOK");
         this.user = (User) intent.getSerializableExtra("USER");
+
+        setContentView(R.layout.activity_view_book);
 
         bookNameTextView = findViewById(R.id.ViewBookName);
         authorTextView = findViewById(R.id.ViewAuthor);
@@ -34,5 +37,11 @@ public class ViewBookActivity extends AppCompatActivity {
         authorTextView.setText(book.getAuthor());
         dateTextView.setText(book.getDate());
         isbnTextView.setText(book.getIsbn());
+
+        //TODO !: imageView
+
+        //TODO 2: ViewBookActivity with 4 kinds of interaction
+
+        //TODO 3: ViewBookActivity conducted by the owner of the book
     }
 }
