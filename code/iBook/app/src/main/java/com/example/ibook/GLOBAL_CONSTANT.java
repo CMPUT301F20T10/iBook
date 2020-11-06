47e0b5be14e3e8ac4d9899726a965980f9b84bff
dsunderm <dsunderm@ualberta.ca>
2020-11-05 17:01:56 -0700

    remode code folder from my branch

--- code/iBook/app/src/main/java/com/example/ibook/GLOBAL_CONSTANT.java
+++
@@ -1,9 +0,0 @@
-package com.example.ibook;
-
-public class GLOBAL_CONSTANT {
-    public static final String MAIN_ACTIVITY = "MAIN_ACTIVITY";
-    public static final String ADD_BOOK_ACTIVITY = "ADD_BOOK_ACTIVITY";
-    public static final String PAGE_ACTIVITY = "PAGE_ACTIVITY";
-    public static final String SCAN_ACTIVITY = "SCAN_ACTIVITY";
-    public static final String VIEW_BOOK_ACTIVITY = "VIEW_BOOK_ACTIVITY";
-}
